3bc36612aa8ca5ced18f4af2ce3698ceaa528ce8
danleyb2 <ndieksman@gmail.com>
2015-11-25 16:35:17 +0300

    rewrite whole script into an object

--- asearch.js
+++ asearch.js
@@ -1,141 +1,112 @@
-var XMLHTTPObject=new XMLHttpRequest();
-var History={};
+var searchAjax = {
+    History: {},
+    useHistory:true,
 
+    init: function (config) {
 
+        this.input = document.querySelector(config.input);
+        this.resultStream = document.querySelector(config.resultsHolder);
+        this.template = config.template;
+        this.url=config.url;
 
-
-function render(template,values){
-     var tags=template.match(/{([a-z1-9\s]+)+}/g);
-     var keys=tags.map(function (v) {
-        return v.replace(/{|}/g,'');
-     });
-
-    //tags.forEach(function (tag) {
-        for(var i=0;i<tags.length;i++) {
-            template=template.replace(tags[i], values[keys[i].trim()] );
+        if(this.input==null||this.resultStream==null||this.template==null||this.url==null){}
+        else {
+            this.XMLHTTPObject = new XMLHttpRequest();
+            this.input.onkeydown = this.keyListener;
+            this.input.addEventListener('input', this.textInput);
+            this.input.addEventListener('blur', function (evt) {
+                //clear history
+            });
         }
-    //});
+    },
+    keyListener: function (evt) {
+        switch (evt.keyCode) {
+            case 38:
+                evt.preventDefault();
+                searchAjax.activePrev(evt);
+                break;
+            case 40:
+                evt.preventDefault();
+                searchAjax.activeNext(evt);
+                //return true;
+                break;
+        }
+    },
+    textInput: function (evt) {
+        var q = evt.target.value.trim();
+        if (q == '')return searchAjax.updateDisplay([]);
+        if (searchAjax.History[q] === undefined) {
+            searchAjax.search(q);
+        } else {
+            console.log('from history');
+            searchAjax.updateDisplay(searchAjax.History[q])
+        }
+    },
+    activePrev: function (evt) {
+        var active = searchAjax.resultStream.querySelector('.active');
+        var prev = active.previousSibling;
+        if (prev === null) {
+        } else {
+            active.classList.remove('active');
+            prev.classList.add('active');
+            searchAjax.updateInput(prev);
+            prev.scrollIntoView(false);
+        }
 
-    return htmlDom(template);
-}
+    },
+    activeNext: function (evt) {
+        var active = searchAjax.resultStream.querySelector('.active');
+        var next = active.nextSibling;
+        if (next === null) {
+        } else {
+            active.classList.remove('active');
+            next.classList.add('active');
+            searchAjax.updateInput(next);
+            next.scrollIntoView(false);
 
-var rTemplate=
-    '<section class="opener">'+
-        '<div class="avatar user3">' +
-        '</div> ' +
-        '<div class="postmeta"> ' +'last seen '+
-            '<span class="time">{time}</span>  ' +
-        '</div>   ' +
-        '<span class="name">{name }</span>' +
-        '<span class="location">from {location}</span>  ' +
-        '<p>{about}</p> ' +
-    '</section>';
+        }
 
-//var resultTemplate=document.createElement('section');
-//resultTemplate.classList.add('opener');
-//resultTemplate.innerHTML=rTemplate;
 
-var htmlDom= function (html) {
-    var el=document.createElement('div');
-    el.innerHTML=html;
-    return el.childNodes[0];
+    },
+    updateInput: function (a) {
+        searchAjax.input.value = a.querySelector('.name').textContent;
+    },
+    render: function (template, values) {
+        var tags = template.match(/{([a-z1-9\s]+)+}/g);
+        var keys = tags.map(function (v) {
+            return v.replace(/{|}/g, '');
+        });
+        for (var i = 0; i < tags.length; i++) {
+            template = template.replace(tags[i], values[keys[i].trim()]);
+        }
+        return searchAjax.toHtmlDom(template);
+    },
+    toHtmlDom: function (html) {
+        var el = document.createElement('div');
+        el.innerHTML = html;
+        return el.childNodes[0];
+    },
+
+    updateDisplay: function (data) {
+        searchAjax.resultStream.innerHTML = '';
+        data.forEach(function (rst) {
+            searchAjax.resultStream.appendChild(searchAjax.render(searchAjax.template, rst));
+        });
+        if (data.length)searchAjax.resultStream.firstChild.classList.add('active');
+    },
+    search: function (q) {
+
+        if (searchAjax.XMLHTTPObject.readyState == 0 || searchAjax.XMLHTTPObject.readyState == 4) {
+            searchAjax.XMLHTTPObject.open('GET', searchAjax.url + q);
+            searchAjax.XMLHTTPObject.onreadystatechange = function () {
+                if (searchAjax.XMLHTTPObject.status == 200 && searchAjax.XMLHTTPObject.readyState == 4) {
+                    var response = JSON.parse(searchAjax.XMLHTTPObject.responseText);
+                    if(searchAjax.useHistory)searchAjax.History[q] = response;
+                    searchAjax.updateDisplay(response);
+                }
+            };
+            searchAjax.XMLHTTPObject.send();
+        }
+    }
+
 };
-
-
-var form;
-var input;
-var resultStream;
-window.onload= function () {
-    form=document.querySelector('#search');
-    resultStream=document.querySelector('.result-stream');
-    //document.onkeydown=keyListener;
-
-    //rTemplate=document.querySelector('#template').cloneNode(true);
-    //rTemplate.removeAttribute('style');
-    //form.addEventListener('submit',search);
-
-    input=form.querySelector('input');
-    input.onkeydown=keyListener;
-    //input.addEventListener('change',textChange);
-    input.addEventListener('input',textChange);
-    input.addEventListener('blur', function (evt) {
-        //clear history
-    });
-
-
-};
-function keyListener(evt){
-    switch (evt.keyCode){
-        case 38:
-            evt.preventDefault();
-            activePrev(evt);
-            break;
-        case 40:
-            evt.preventDefault();
-            activeNext(evt);
-            //return true;
-            break;
-    }
-}
-function activePrev(evt){
-    var active=resultStream.querySelector('.active');
-    var prev=active.previousSibling;
-    if(prev===null){}else {
-        active.classList.remove('active');
-        prev.classList.add('active');
-        updateInput(prev);
-        prev.scrollIntoView(false);
-
-    }
-
-
-}
-function activeNext(evt){
-    var active=resultStream.querySelector('.active');
-    var next=active.nextSibling;
-    if(next===null){}else {
-        active.classList.remove('active');
-        next.classList.add('active');
-        updateInput(next);
-        next.scrollIntoView(false);
-
-    }
-
-
-}
-function updateInput(a){
-    input.value=a.querySelector('.name').textContent;
-}
-function updateDisplay(data){
-    resultStream.innerHTML='';
-    data.forEach(function (rst) {
-        resultStream.appendChild(render(rTemplate,rst));
-    });
-    if(data.length)resultStream.firstChild.classList.add('active');
-}
-
-function textChange(evt){
-    var q=input.value.trim();
-    if(q=='')return updateDisplay([]);
-    if(History[q]===undefined){
-        search(q);
-    }else{
-       updateDisplay(History[q])
-    }
-}
- function search (q) {
-
-    if(XMLHTTPObject.readyState==0||XMLHTTPObject.readyState==4){
-        XMLHTTPObject.open('GET','search.php?q='+q);
-        XMLHTTPObject.onreadystatechange= function () {
-          if(XMLHTTPObject.status==200 && XMLHTTPObject.readyState==4){
-              var response=JSON.parse(XMLHTTPObject.responseText);
-              History[q]=response;
-              updateDisplay(response);
-          }
-        };
-        XMLHTTPObject.send();
-    }
-}
-
-
